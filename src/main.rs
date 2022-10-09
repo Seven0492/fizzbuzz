b42dacff914ff44e65a2ac06ff8621e54792a26f
Seven <sethm6348@gmail.com>
2022-10-09 16:06:26 -0400

    v1.1

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    for i in 1..101 {
+        if i % 3 == 0 && i % 5 == 0 {
+            println!("FizzBuzz");
+            continue;
+        }
+
+        if i % 3 == 0 {
+            println!("Fizz");
+            continue;
+        }
+
+        if i % 5 == 0 {
+            println!("Buzz");
+            continue;
+        }
+
+        println!("{}", i);
+    }
+}
